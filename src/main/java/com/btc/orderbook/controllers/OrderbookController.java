06fb6758828f4df99b0db9ac30785f0bf30662fd
Vinicius Soares <47466580+vinicius-gomes-dev@users.noreply.github.com>
2022-02-14 03:44:42 -0300

    Update OrderbookController.java

--- src/main/java/com/btc/orderbook/controllers/OrderbookController.java
+++ src/main/java/com/btc/orderbook/controllers/OrderbookController.java
@@ -1,6 +1,7 @@
 package com.btc.orderbook.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +13,10 @@ public class OrderbookController {
 	
 	@Autowired
 	private OrderbookService service;
+	
+	@GetMapping
+	public String tempMethod() {
+		return "Book route";
+	}
 
 }
